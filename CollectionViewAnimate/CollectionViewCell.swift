c46dba7ef51b54803d53fac4a7b8ec92bd3f3800
claudioitalian12 <dark.claudio12@gmail.com>
2018-10-24 17:00:00 +0200

    CollectionView Animation

---
+++ CollectionViewAnimate/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  TimeKick
+//
+//  Created by claudio Cavalli on 20/10/2018.
+//  Copyright © 2018 claudio Cavalli. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+
+    
+}
+
--- CollectionViewAnimate/ViewController.swift
+++ CollectionViewAnimate/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ var image = ["book-2","library","scholarship","management","brainstorm","audio","hourglass"]
+    
+    @IBOutlet weak var collectionview: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        var i = 0
+        let fade = animate()
+        while(i<7){
+            (collectionview.cellForItem(at: IndexPath(item: i, section: 0))?.layer)?.add(fade, forKey: nil)
+            i = i + 1
+            fade.beginTime = CACurrentMediaTime() + (Double(i)/5)
+            
+        }
+    }
+    
+    func animate() -> CABasicAnimation{
+        let fadeIn = CABasicAnimation(keyPath: "opacity")
+        fadeIn.fromValue = 0.0
+        fadeIn.toValue = 1.0
+        fadeIn.duration = 1.0
+        fadeIn.fillMode = CAMediaTimingFillMode.backwards
+        
+        return fadeIn
+    }
 
 }
 
